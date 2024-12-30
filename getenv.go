e7356b1dec0a69c05c4d3424fa3dd6adad107365
Uğur Özyılmazel <ugurozyilmazel@gmail.com>
2024-12-30 16:10:22 +0300

    wip -

--- getenv.go
+++ getenv.go
@@ -3,7 +3,6 @@ package getenv
 import (
 	"fmt"
 	"os"
-	"strings"
 )
 
 // StringVerifyFunc is a verify function type for String.
@@ -29,55 +28,96 @@ func String(name, defaultValue string, vf StringVerifyFunc) (string, error) {
 	return val, nil
 }
 
-// BoolVerifyFunc is a verify function type for Bool.
-type BoolVerifyFunc func(bool) error
+// // BoolVerifyFunc is a verify function type for Bool.
+// type BoolVerifyFunc func(bool) error
+//
+// // Bool retrieves the value of the environment variable identified by the given
+// // name as a boolean. If the environment variable is not set, it falls back to
+// // the provided defaultValue. Optionally, a custom validation function (vf) of
+// // type BoolVerifyFunc can be supplied to perform additional checks on the
+// // resulting boolean value. If the validation fails, an error is returned.
+// func Bool(name string, defaultValue any, vf BoolVerifyFunc) (bool, error) {
+// 	val := os.Getenv(name)
+// 	if val == "" {
+// 		defVal := parseBool(defaultValue)
+// 		if vf != nil {
+// 			if err := vf(defVal); err != nil {
+// 				return false, fmt.Errorf("verify error: %w", err)
+// 			}
+// 		}
+//
+// 		return defVal, nil
+// 	}
+//
+// 	val = strings.ToLower(val)
+//
+// 	return isTrue(val), nil
+// }
+//
+// func parseBool(val any) bool {
+// 	switch v := val.(type) {
+// 	case bool:
+// 		return v
+// 	case string:
+// 		return isTrue(strings.ToLower(v))
+// 	default:
+// 		return false
+// 	}
+// }
+//
+// func isTrue(value string) bool {
+// 	trueValues := []string{"1", "t", "true", "T", "True", "TRUE"}
+//
+// 	return contains(trueValues, value)
+// }
+//
+// func contains(slice []string, value string) bool {
+// 	for _, v := range slice {
+// 		if v == value {
+// 			return true
+// 		}
+// 	}
+//
+// 	return false
+// }
 
-// Bool retrieves the value of the environment variable identified by the given
-// name as a boolean. If the environment variable is not set, it falls back to
-// the provided defaultValue. Optionally, a custom validation function (vf) of
-// type BoolVerifyFunc can be supplied to perform additional checks on the
-// resulting boolean value. If the validation fails, an error is returned.
-func Bool(name string, defaultValue any, vf BoolVerifyFunc) (bool, error) {
-	val := os.Getenv(name)
-	if val == "" {
-		defVal := parseBool(defaultValue)
-		if vf != nil {
-			if err := vf(defVal); err != nil {
-				return false, fmt.Errorf("verify error: %w", err)
-			}
-		}
-
-		return defVal, nil
-	}
-
-	val = strings.ToLower(val)
-
-	return isTrue(val), nil
-}
-
-func parseBool(val any) bool {
-	switch v := val.(type) {
-	case bool:
-		return v
-	case string:
-		return isTrue(strings.ToLower(v))
-	default:
-		return false
-	}
-}
-
-func isTrue(value string) bool {
-	trueValues := []string{"1", "t", "true", "T", "True", "TRUE"}
-
-	return contains(trueValues, value)
-}
-
-func contains(slice []string, value string) bool {
-	for _, v := range slice {
-		if v == value {
-			return true
-		}
-	}
-
-	return false
-}
+// // IntVerifyFunc is a verify function type for Bool.
+// type IntVerifyFunc func(int) error
+//
+// // Int ...
+// func Int(name string, defaultValue any, vf IntVerifyFunc) (int, error) {
+// 	val := os.Getenv(name)
+// 	if val == "" {
+// 		defVal := parseInt(defaultValue)
+// 		if vf != nil {
+// 			if err := vf(defVal); err != nil {
+// 				return 0, fmt.Errorf("verify error: %w", err)
+// 			}
+// 		}
+//
+// 		return defVal, nil
+// 	}
+//
+// 	i, err := strconv.Atoi(val)
+// 	if err != nil {
+// 		return 0, fmt.Errorf("convert error: %w", err)
+// 	}
+//
+// 	return i, nil
+// }
+//
+// func parseInt(val any) int {
+// 	switch v := val.(type) {
+// 	case int:
+// 		return v
+// 	case string:
+// 		i, err := strconv.Atoi(v)
+// 		if err != nil {
+// 			return 0
+// 		}
+//
+// 		return i
+// 	default:
+// 		return 0
+// 	}
+// }
--- getenv_test.go
+++ getenv_test.go
@@ -69,82 +69,181 @@ func TestString(t *testing.T) {
 	}
 }
 
-func ExampleString_non_existing_var_without_error() {
-	value, _ := getenv.String("NON_EXISTENT", "default-value", nil)
-	fmt.Println(value)
-	// Output:
-	// default-value
-}
+// func ExampleString_non_existing_var_without_error() {
+// 	value, _ := getenv.String("NON_EXISTENT", "default-value", nil)
+// 	fmt.Println(value)
+// 	// Output:
+// 	// default-value
+// }
 
-func TestBool(t *testing.T) {
-	os.Setenv("TEST_BOOL_1", "TRUE")
-	os.Unsetenv("TEST_BOOL_NON_EXISTENT")
-	defer func() { os.Unsetenv("TEST_BOOL_1") }()
+// func TestBool(t *testing.T) {
+// 	os.Setenv("TEST_BOOL_1", "TRUE")
+// 	os.Setenv("TEST_BOOL_2", "True")
+// 	os.Setenv("TEST_BOOL_3", "true")
+// 	os.Setenv("TEST_BOOL_4", "T")
+// 	os.Setenv("TEST_BOOL_5", "t")
+//
+// 	os.Unsetenv("TEST_BOOL_NON_EXISTENT")
+// 	defer func() {
+// 		os.Unsetenv("TEST_BOOL_1")
+// 		os.Unsetenv("TEST_BOOL_2")
+// 		os.Unsetenv("TEST_BOOL_3")
+// 		os.Unsetenv("TEST_BOOL_4")
+// 		os.Unsetenv("TEST_BOOL_5")
+// 	}()
+//
+// 	testcases := []struct {
+// 		testName    string
+// 		envName     string
+// 		defaultVal  any
+// 		verifyFunc  getenv.BoolVerifyFunc
+// 		expectedVal bool
+// 		expectedErr error
+// 	}{
+// 		{
+// 			testName:    "test TEST_BOOL_1 with TRUE value",
+// 			envName:     "TEST_BOOL_1",
+// 			defaultVal:  "False",
+// 			verifyFunc:  nil,
+// 			expectedVal: true,
+// 			expectedErr: nil,
+// 		},
+// 		{
+// 			testName:    "test TEST_BOOL_2 with True value",
+// 			envName:     "TEST_BOOL_2",
+// 			defaultVal:  "",
+// 			verifyFunc:  nil,
+// 			expectedVal: true,
+// 			expectedErr: nil,
+// 		},
+// 		{
+// 			testName:    "test TEST_BOOL_3 with true value",
+// 			envName:     "TEST_BOOL_3",
+// 			defaultVal:  nil,
+// 			verifyFunc:  nil,
+// 			expectedVal: true,
+// 			expectedErr: nil,
+// 		},
+// 		{
+// 			testName:    "test TEST_BOOL_4 with T value",
+// 			envName:     "TEST_BOOL_4",
+// 			defaultVal:  nil,
+// 			verifyFunc:  nil,
+// 			expectedVal: true,
+// 			expectedErr: nil,
+// 		},
+// 		{
+// 			testName:    "test TEST_BOOL_t with t value",
+// 			envName:     "TEST_BOOL_5",
+// 			defaultVal:  nil,
+// 			verifyFunc:  nil,
+// 			expectedVal: true,
+// 			expectedErr: nil,
+// 		},
+// 		{
+// 			testName:    "test TEST_BOOL_NON_EXISTENT with default F value",
+// 			envName:     "TEST_BOOL_NON_EXISTENT",
+// 			defaultVal:  "F",
+// 			verifyFunc:  nil,
+// 			expectedVal: false,
+// 			expectedErr: nil,
+// 		},
+// 		{
+// 			testName:    "test TEST_BOOL_NON_EXISTENT with default True value",
+// 			envName:     "TEST_BOOL_NON_EXISTENT",
+// 			defaultVal:  "True",
+// 			verifyFunc:  nil,
+// 			expectedVal: true,
+// 			expectedErr: nil,
+// 		},
+// 		{
+// 			testName:   "test TEST_BOOL_NON_EXISTENT with default errorious value",
+// 			envName:    "TEST_BOOL_NON_EXISTENT",
+// 			defaultVal: "invalid",
+// 			verifyFunc: func(val bool) error {
+// 				if !val {
+// 					return fmt.Errorf("%w", errTestOnly)
+// 				}
+// 				return nil
+// 			},
+// 			expectedVal: false,
+// 			expectedErr: errTestOnly,
+// 		},
+// 		{
+// 			testName:    "test non existing env-var with default bool value",
+// 			envName:     "TEST_BOOL_NON_EXISTENT",
+// 			defaultVal:  true,
+// 			verifyFunc:  nil,
+// 			expectedVal: true,
+// 			expectedErr: nil,
+// 		},
+// 		{
+// 			testName:    "test non existing env-var with default int value",
+// 			envName:     "TEST_BOOL_NON_EXISTENT",
+// 			defaultVal:  999,
+// 			verifyFunc:  nil,
+// 			expectedVal: false,
+// 			expectedErr: nil,
+// 		},
+// 	}
+//
+// 	for _, tc := range testcases {
+// 		t.Run(tc.testName, func(t *testing.T) {
+// 			val, err := getenv.Bool(tc.envName, tc.defaultVal, tc.verifyFunc)
+// 			if !errors.Is(err, tc.expectedErr) {
+// 				t.Errorf("want: %v, got: %v", tc.expectedErr, err)
+// 			}
+// 			if val != tc.expectedVal {
+// 				t.Errorf("want: %t, got: %t", tc.expectedVal, val)
+// 			}
+// 		})
+// 	}
+// }
 
-	testcases := []struct {
-		testName    string
-		envName     string
-		defaultVal  any
-		verifyFunc  getenv.BoolVerifyFunc
-		expectedVal bool
-		expectedErr error
-	}{
-		{
-			testName:    "test existing env-var with TRUE value",
-			envName:     "TEST_BOOL_1",
-			defaultVal:  "False",
-			verifyFunc:  nil,
-			expectedVal: true,
-			expectedErr: nil,
-		},
-		{
-			testName:    "test non existing env-var with default True value",
-			envName:     "TEST_BOOL_NON_EXISTENT",
-			defaultVal:  "True",
-			verifyFunc:  nil,
-			expectedVal: true,
-			expectedErr: nil,
-		},
-		{
-			testName:   "test non existing env-var with default errorious value",
-			envName:    "TEST_BOOL_NON_EXISTENT",
-			defaultVal: "invalid",
-			verifyFunc: func(val bool) error {
-				if !val {
-					return fmt.Errorf("%w", errTestOnly)
-				}
-				return nil
-			},
-			expectedVal: false,
-			expectedErr: errTestOnly,
-		},
-		{
-			testName:    "test non existing env-var with default bool value",
-			envName:     "TEST_BOOL_NON_EXISTENT",
-			defaultVal:  true,
-			verifyFunc:  nil,
-			expectedVal: true,
-			expectedErr: nil,
-		},
-		{
-			testName:    "test non existing env-var with default int value",
-			envName:     "TEST_BOOL_NON_EXISTENT",
-			defaultVal:  999,
-			verifyFunc:  nil,
-			expectedVal: false,
-			expectedErr: nil,
-		},
-	}
-
-	for _, tc := range testcases {
-		t.Run(tc.testName, func(t *testing.T) {
-			val, err := getenv.Bool(tc.envName, tc.defaultVal, tc.verifyFunc)
-			if !errors.Is(err, tc.expectedErr) {
-				t.Errorf("want: %v, got: %v", tc.expectedErr, err)
-			}
-			if val != tc.expectedVal {
-				t.Errorf("want: %t, got: %t", tc.expectedVal, val)
-			}
-		})
-	}
-}
+// func TestInt(t *testing.T) {
+// 	os.Setenv("TEST_INT_1", "1")
+// 	os.Setenv("TEST_INT_2", "invalid")
+// 	os.Unsetenv("TEST_INT_NON_EXISTENT")
+//
+// 	defer func() {
+// 		os.Unsetenv("TEST_INT_1")
+// 	}()
+//
+// 	testcases := []struct {
+// 		testName    string
+// 		envName     string
+// 		defaultVal  any
+// 		verifyFunc  getenv.IntVerifyFunc
+// 		expectedVal int
+// 		expectedErr error
+// 	}{
+// 		{
+// 			testName:    "test TEST_INT_1 with '1' value",
+// 			envName:     "TEST_INT_1",
+// 			defaultVal:  nil,
+// 			verifyFunc:  nil,
+// 			expectedVal: 1,
+// 			expectedErr: nil,
+// 		},
+// 		{
+// 			testName:    "test TEST_INT_2 with 'invalid' value",
+// 			envName:     "TEST_INT_2",
+// 			defaultVal:  nil,
+// 			verifyFunc:  nil,
+// 			expectedVal: 0,
+// 			expectedErr: nil,
+// 		},
+// 	}
+//
+// 	for _, tc := range testcases {
+// 		t.Run(tc.testName, func(t *testing.T) {
+// 			val, err := getenv.Int(tc.envName, tc.defaultVal, tc.verifyFunc)
+// 			if !errors.Is(err, tc.expectedErr) {
+// 				t.Errorf("want: %v, got: %v", tc.expectedErr, err)
+// 			}
+// 			if val != tc.expectedVal {
+// 				t.Errorf("want: %d, got: %d", tc.expectedVal, val)
+// 			}
+// 		})
+// 	}
+// }
